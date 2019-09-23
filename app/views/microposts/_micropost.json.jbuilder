55b949bf41e0915a8b68a4f9751987d4a61ca333
N.mickey <mickey.1n13m4@gmail.com>
2019-09-24 04:50:15 +0900

    conflict解消

--- app/views/microposts/_micropost.json.jbuilder
+++ app/views/microposts/_micropost.json.jbuilder
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 json.extract! micropost, :id, :content, :user_id, :created_at, :updated_at
-=======
-json.extract! micropost, :id, :context, :user_id, :created_at, :updated_at
->>>>>>> 88f2e9e19b3ff64e64ab47ec4f55187e165d9aa1
 json.url micropost_url(micropost, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   resources :microposts
   resources :users
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
  	# For details on the DSL available within this file
  	# see http://guides.rubyonrails.org/routing.html
 	root 'users#index'
->>>>>>> 88f2e9e19b3ff64e64ab47ec4f55187e165d9aa1
 end
